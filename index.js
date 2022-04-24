4a7d87e7abeb21a73753285a70fb334f0b75f581
VictorDionizio <victordlima002@gmail.com>
2022-04-24 09:14:30 -0300

    added js-file

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const blacktheme = document.getElementById("darkTheme");
+
+
+
+function darkmode() {
+    var element = document.body;
+    var sidebar = document.getElementById("sidebar");
+    var sidebar2 = document.getElementById("sidebar2");
+    element.classList.toggle("darkmodeBody");
+    sidebar.classList.toggle("leftandright");
+    sidebar2.classList.toggle("leftandright")
+    
+    var luahover = document.getElementById("hoverlua");
+    
+  }
+
+
+
+
